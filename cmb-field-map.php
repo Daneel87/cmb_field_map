3299d3293fd917ead7a26374f6250028dfa5fd63
dbrock89 <33625781+dbrock89@users.noreply.github.com>
2018-12-10 23:06:28 -0800

    Update cmb-field-map.php

--- cmb-field-map.php
+++ cmb-field-map.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: CMB2 Field Type: Google Maps
-Plugin URI: https://github.com/mustardBees/cmb_field_map
-GitHub Plugin URI: https://github.com/mustardBees/cmb_field_map
+Plugin URI: https://github.com/dbrock89/cmb_field_map
+GitHub Plugin URI: https://github.com/dbrock89/cmb_field_map
 Description: Google Maps field type for CMB2.
 Version: 2.2.0
-Author: Phil Wylie
-Author URI: https://www.philwylie.co.uk/
+Author: D. Brock
+Author URI: https://github.com/dbrock89
 License: GPLv2+
 */
 
@@ -26,7 +26,7 @@ class PW_CMB2_Field_Google_Maps {
 	public function __construct() {
 		add_filter( 'cmb2_render_pw_map', array( $this, 'render_pw_map' ), 10, 5 );
 		add_filter( 'cmb2_sanitize_pw_map', array( $this, 'sanitize_pw_map' ), 10, 4 );
-		add_filter( 'pw_google_api_key', array( $this, 'google_api_key_constant' ) );
+		add_filter( 'pw_google_api_key', array( $this, 'google_api_key_constant' ));
 	}
 
 	/**
@@ -42,26 +42,125 @@ class PW_CMB2_Field_Google_Maps {
 
 		$this->setup_admin_scripts( $api_key );
 
-		echo '<input type="text" class="large-text pw-map-search" id="' . $field->args( 'id' ) . '" />';
+		echo '<input type="text" class="large-text pw-map-search" id="' . $field->args( 'id' ) . '" placeholder="Type Name of Facility or Location or Address"/>';
 
+		$field_type_object->_desc( false, false );
+
+		if ( $field->args('split_values') === true ) {
+			echo '<br /><br />';
+			echo $field_type_object->input(array(
+				'type'       => 'text',
+				'id' 				 => $field->args( 'id' ) . '_place_name',
+				'name'       => $field->args('_name') . '[place_name]',
+				'value'      => isset( $field_escaped_value['place_name'] ) ? $field_escaped_value['place_name'] : '',
+				'class'      => 'pw-map-place_name',
+				'placeholder'       => 'Facility Name',
+			));
+			echo '<br />';
+			echo $field_type_object->input(array(
+				'type'       => 'text',
+				'id' 				 => $field->args( 'id' ) . '_street_number',
+				'name'       => $field->args('_name') . '[street_number]',
+				'value'      => isset( $field_escaped_value['street_number'] ) ? $field_escaped_value['street_number'] : '',
+				'class'      => 'pw-map-street_number',
+				'placeholder'       => 'Street Number',
+			));
+			echo $field_type_object->input(array(
+				'type'       => 'text',
+				'id' 				 => $field->args( 'id' ) . '_route',
+				'name'       => $field->args('_name') . '[route]',
+				'value'      => isset( $field_escaped_value['route'] ) ? $field_escaped_value['route'] : '',
+				'class'      => 'pw-map-route',
+				'placeholder'       => 'Route',
+			));
+			echo $field_type_object->input(array(
+				'type'       => 'text',
+				'id' 				 => $field->args( 'id' ) . '_unit',
+				'name'       => $field->args('_name') . '[unit]',
+				'value'      => isset( $field_escaped_value['unit'] ) ? $field_escaped_value['unit'] : '',
+				'class'      => 'pw-map-unit',
+				'placeholder'       => 'Bldg. / Room # / Sub-Location',
+			));
+			echo '<br />';
+			echo $field_type_object->input(array(
+				'type'       => 'text',
+				'id' 				 => $field->args( 'id' ) . '_locality',
+				'name'       => $field->args('_name') . '[locality]',
+				'value'      => isset( $field_escaped_value['locality'] ) ? $field_escaped_value['locality'] : '',
+				'class'      => 'pw-map-locality',
+				'placeholder'       => 'Locality (City)',
+			));
+			echo $field_type_object->input(array(
+				'type'       => 'text',
+				'id' 				 => $field->args( 'id' ) . '_administrative_area_level_1',
+				'name'       => $field->args('_name') . '[administrative_area_level_1]',
+				'value'      => isset( $field_escaped_value['administrative_area_level_1'] ) ? $field_escaped_value['administrative_area_level_1'] : '',
+				'class'      => 'pw-map-administrative_area_level_1',
+				'placeholder'       => 'Administrative Area Level 1 (State)',
+			));
+			echo $field_type_object->input(array(
+				'type'       => 'text',
+				'id' 				 => $field->args( 'id' ) . '_postal_code',
+				'name'       => $field->args('_name') . '[postal_code]',
+				'value'      => isset( $field_escaped_value['postal_code'] ) ? $field_escaped_value['postal_code'] : '',
+				'class'      => 'pw-map-postal_code',
+				'placeholder'       => 'Postal Code',
+			));
+			echo '<br />';
+			echo $field_type_object->input(array(
+				'type'       => 'text',
+				'id' 				 => $field->args( 'id' ) . '_country',
+				'name'       => $field->args('_name') . '[country]',
+				'value'      => isset( $field_escaped_value['country'] ) ? $field_escaped_value['country'] : '',
+				'class'      => 'pw-map-country',
+				'placeholder'       => 'Country',
+			));
+			echo $field_type_object->input(array(
+				'type'       => current_user_can( 'manage_options' ) ? 'text' : 'hidden',
+				'id' 				 => $field->args( 'id' ) . '_utc_offset',
+				'name'       => $field->args('_name') . '[utc_offset]',
+				'value'      => isset( $field_escaped_value['utc_offset'] ) ? $field_escaped_value['utc_offset'] : '',
+				'class'      => 'pw-map-utc_offset',
+				'placeholder'       => 'UTC Offset',
+			));
+			echo '<br /><br />';
+		}
 		echo '<div class="pw-map"></div>';
+		echo '<br />';
+		echo $field_type_object->input(array(
+			'type'       => current_user_can( 'manage_options' ) ? 'text' : 'hidden',
+			'id' 				 => $field->args( 'id' ) . '_lat',
+			'name'       => $field->args('_name') . '[lat]',
+			'value'      => isset( $field_escaped_value['lat'] ) ? $field_escaped_value['lat'] : '',
+			'class'      => 'pw-map-lat',
+			'placeholder'       => 'Latitude',
+		));
+		echo $field_type_object->input(array(
+			'type'       => current_user_can( 'manage_options' ) ? 'text' : 'hidden',
+			'id' 				 => $field->args( 'id' ) . '_lng',
+			'name'       => $field->args('_name') . '[lng]',
+			'value'      => isset( $field_escaped_value['lng'] ) ? $field_escaped_value['lng'] : '',
+			'class'      => 'pw-map-lng',
+			'placeholder'       => 'Longitude',
+		));
+		echo $field_type_object->input(array(
+			'type'       => current_user_can( 'manage_options' ) ? 'text' : 'hidden',
+			'id' 				 => $field->args( 'id' ) . '_formatted_address',
+			'name'       => $field->args('_name') . '[formatted_address]',
+			'value'      => isset( $field_escaped_value['formatted_address'] ) ? $field_escaped_value['formatted_address'] : '',
+			'class'      => 'pw-map-formatted_address',
+			'placeholder'       => 'Formated Address',
+		));
 
-		$field_type_object->_desc( true, true );
+		if ( $field->args('map_drawings') === true ) {
+			echo $field_type_object->input(array(
+				'type'       => 'hidden',
+				'id' 				 => $field->args( 'id' ) . '_drawing_manager',
+				'name'       => $field->args('_name') . '[drawing_manager]',
+				'class'      => 'pw-map-drawing_manager',
+			));
+		}
 
-		echo $field_type_object->input( array(
-			'type'       => 'hidden',
-			'name'       => $field->args('_name') . '[latitude]',
-			'value'      => isset( $field_escaped_value['latitude'] ) ? $field_escaped_value['latitude'] : '',
-			'class'      => 'pw-map-latitude',
-			'desc'       => '',
-		) );
-		echo $field_type_object->input( array(
-			'type'       => 'hidden',
-			'name'       => $field->args('_name') . '[longitude]',
-			'value'      => isset( $field_escaped_value['longitude'] ) ? $field_escaped_value['longitude'] : '',
-			'class'      => 'pw-map-longitude',
-			'desc'       => '',
-		) );
 	}
 
 	/**
@@ -69,12 +168,52 @@ class PW_CMB2_Field_Google_Maps {
 	 */
 	public function sanitize_pw_map( $override_value, $value, $object_id, $field_args ) {
 		if ( isset( $field_args['split_values'] ) && $field_args['split_values'] ) {
-			if ( ! empty( $value['latitude'] ) ) {
-				update_post_meta( $object_id, $field_args['id'] . '_latitude', $value['latitude'] );
+			if ( ! empty( $value['lat'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_lat', $value['lat'] );
 			}
 
-			if ( ! empty( $value['longitude'] ) ) {
-				update_post_meta( $object_id, $field_args['id'] . '_longitude', $value['longitude'] );
+			if ( ! empty( $value['lng'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_lng', $value['lng'] );
+			}
+
+			if ( ! empty( $value['formatted_address'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_formatted_address', $value['formatted_address'] );
+			}
+
+			if ( ! empty( $value['utc_offset'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_utc_offset', $value['utc_offset'] );
+			}
+
+			if ( ! empty( $value['place_name'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_place_name', $value['place_name'] );
+			}
+
+			if ( ! empty( $value['street_number'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_street_number', $value['street_number'] );
+			}
+
+			if ( ! empty( $value['route'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_route', $value['route'] );
+			}
+
+			if ( ! empty( $value['unit'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_unit', $value['unit'] );
+			}
+
+			if ( ! empty( $value['locality'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_locality', $value['locality'] );
+			}
+
+			if ( ! empty( $value['administrative_area_level_1'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_administrative_area_level_1', $value['administrative_area_level_1'] );
+			}
+
+			if ( ! empty( $value['postal_code'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_postal_code', $value['postal_code'] );
+			}
+
+			if ( ! empty( $value['country'] ) ) {
+				update_post_meta( $object_id, $field_args['id'] . '_country', $value['country'] );
 			}
 		}
 
@@ -85,7 +224,7 @@ class PW_CMB2_Field_Google_Maps {
 	 * Enqueue scripts and styles.
 	 */
 	public function setup_admin_scripts($api_key) {
-		wp_register_script( 'pw-google-maps-api', "https://maps.googleapis.com/maps/api/js?key={$api_key}&libraries=places", null, null );
+		wp_register_script( 'pw-google-maps-api', "https://maps.googleapis.com/maps/api/js?key={$api_key}&libraries=drawing,places,geometry", null, null );
 		wp_enqueue_script( 'pw-google-maps', plugins_url( 'js/script.js', __FILE__ ), array( 'pw-google-maps-api', 'jquery' ), self::VERSION );
 		wp_enqueue_style( 'pw-google-maps', plugins_url( 'css/style.css', __FILE__ ), array(), self::VERSION );
 	}
